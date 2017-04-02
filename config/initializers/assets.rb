bbb47f2355b622d0ed3d7d3e3849240e85b5f978
xgironx <xgironx@gmail.com>
2017-04-02 09:08:28 -0400

    turning precompile off

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,15 @@
+
+                              # ###############################
+                              # 201704020837L   EL SABADO   JAY Y EVS A FOO
+                              # The Rails "Asset Pipeline" loads, consolidates, and minifies assets (Javascript, CSS, etc). Out-of-the-box, it takes all your Javascript and puts it in one file, and takes all your CSS and puts it in one file. This is called "precompiling".
+                  # Instructor Notes
+                  # Configure Rails to serve our custom static assets
+                  # in  config/initializers/assets.rb
+                  # # Rails.application.config.assets.precompile += %w( products.js products.css )
+# Rails.application.config.assets.precompile += %w( stallions.js stallions.css )
+
+config.assets.initialize_on_precompile = false
+
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
